d5d22c01c21e8caed4dd019ecf40e753ac1dad37
OdhisGaga <nyolalmakaburi@gmail.com>
2025-08-22 23:16:11 +0300

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,7 @@
+module.exports.initiatePayment = async (phone, amount) => {
+  // Simulate Transfi API call
+  console.log(`Sending ${amount} to Transfi account xgagarich@gmail.com`);
+  return { success: true};
+};
+
+  
